e61bbd3bd2019f4b79fd13b15693e9d25a59e71d
Joe Kale <jkale@precisionplanting.com>
2022-12-05 11:12:20 -0600

    day5 python finished

---
+++ 2022/python/day5/d5_2.py
@@ -0,0 +1,43 @@
+import logging
+import re
+
+_RE_ROW_SPLIT = re.compile(r"(?:(?:([(A-Z)]|\s\s\s\s)))")
+_RE_MOVES = re.compile(r"(?:move\s(\d+))\s(?:from\s(\d+))\s(?:to\s(\d+))")
+
+logging.basicConfig(level=logging.INFO)
+
+answer = ""
+stacks = []
+with open("input.txt", "r") as f:
+    parsing = True
+    for line in f:
+        if parsing:
+            l = _RE_ROW_SPLIT.findall(line.rstrip())
+            if len(l) == 0:
+                parsing = False
+                logging.warning(f"stack post parsing: {stacks}")
+
+            for i, val in enumerate(l):
+                if len(stacks) <= i:
+                    stacks.append(list())
+
+                if val.strip() != '': 
+                    stacks[i].insert(0, val)
+
+        else:
+            l = _RE_MOVES.findall(line.strip())
+            logging.debug(f"{l}")
+            if len(l) == 0:
+                continue
+            else:
+                l = [int(g) for g in list(l[0])];
+                logging.debug(f"{l}")
+                stacks[l[2] - 1] += stacks[l[1] - 1][-l[0]:]
+                stacks[l[1] - 1] = stacks[l[1]- 1][:-l[0]]
+                logging.debug(f"moved: {stacks}")
+
+for stack in stacks:
+    answer += stack.pop() 
+
+logging.info(f"answer = {answer}")
+
